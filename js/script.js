4653de3192615549b1409bbd7bc44136a8ccd432
Davide <ciampad06@gmail.com>
2020-12-09 14:12:45 +0100

    Fine parte 1 ed inizio parte 2

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var app = new Vue(
+  {
+    el: '#root',
+    data: {
+      bookOne: [
+        {
+        	title: "Il vecchio e il mare",
+        	author: "Ernest Hemingway",
+        	year: 1951
+      	}
+      ],
+      bookTwo: [
+        {
+          title: "La forma dell'acqua",
+        	author: "Andrea Camilleri",
+        	year: 1994
+      	}
+      ],
+      bookThree: [
+        {
+          title: "Elogio della follia",
+          author: "Henri Laborit",
+          year: 1976
+      	}
+      ],
+      booksFour: [
+        {
+          title: "La camera azzurra",
+          author: "George Simenon",
+          year: 1964
+      	}
+      ],
+      booksFive: [
+        {
+          title: "",
+          author: "",
+          year: 
+        }
+      ]
+    }
+  }
+);
